5e49eccf2ec4eba2e9949e454f074210f22ba749
C23097798 <mahajak@cardiff.ac.uk>
2025-05-12 11:07:12 +0100

    Initial Commit

---
+++ RecipeApp/RecipeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeAppApp.swift
+//  RecipeApp
+//
+//  Created by Kelvin Mahaja on 12/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct RecipeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
